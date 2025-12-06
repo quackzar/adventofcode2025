614102761b9091bd61ad5832c1976d24988afa6a
Mikkel Wienberg Madsen <mikkel.wienberg@gmail.com>
2025-12-06 01:06:41 +0100

    day 05 😎

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,5 +1,5 @@
 #![feature(iterator_try_collect)]
-use std::collections::BTreeSet;
+use std::collections::{BTreeSet, VecDeque};
 
 fn main() {
     use std::io::Read;
@@ -16,24 +16,74 @@ fn solve1(input: &str) -> u64 {
     let mut ranges = BTreeSet::new();
     let mut input = input.lines();
     for line in input.by_ref() {
-        if line == "\n" { break }
+        if line.is_empty() { break };
         let (begin, end) = line.split_once('-').unwrap();
-        let begin: u32 = begin.parse().unwrap();
-        let end: u32 = end.parse().unwrap();
+        let begin: u64 = begin.parse().unwrap();
+        let end: u64 = end.parse().unwrap();
         ranges.insert((begin, end));
     }
 
-    let ids: Vec<u32> = input.map(str::parse)
+    let ids: Vec<u64> = input.map(str::parse)
         .try_collect().unwrap();
 
+    let mut fresh = 0;
+    for id in ids {
+        for &(begin, end) in ranges.iter() {
+            if begin <= id && id <= end {
+                fresh += 1;
+                break
+            }
+        }
+    }
 
-
-
-    todo!()
+    fresh
 }
 
 fn solve2(input: &str) -> u64 {
-    todo!()
+    let mut ranges = Vec::new();
+    let mut input = input.lines();
+    for line in input.by_ref() {
+        if line.is_empty() { break };
+        let (begin, end) = line.split_once('-').unwrap();
+        let begin: u64 = begin.parse().unwrap();
+        let end: u64 = end.parse().unwrap();
+        ranges.push((begin, end));
+    }
+
+    // optimize
+    let mut disjoint = Vec::new();
+    ranges.sort_by_key(|&(begin, _)|begin);
+    let mut ranges: VecDeque<_> = ranges.into();
+    loop {
+        // invariant: first pair is always starts before second pair
+        let (f0, t0) = ranges.pop_front().unwrap();
+        let Some((f1, t1)) = ranges.pop_front() else {
+            // none left
+            disjoint.push((f0, t0));
+            break
+        };
+        if  t1 <= t0 {
+            // the second range is irrelevant as it is shorter
+            ranges.push_front((f0, t0));
+        } else if f1 <= t0 {
+            // ranges can be combined
+            ranges.push_front((f0, t1));
+        } else if t0 < f1 {
+            // ranges are disjoint
+            disjoint.push((f0, t0));
+            // retry second
+            ranges.push_front((f1, t1));
+        }
+    }
+
+    let mut total_fresh =  0;
+    for (begin, end) in disjoint {
+        // add one since they are inclusive
+        println!("{begin}-{end}");
+        total_fresh += end - begin + 1;
+    }
+
+    total_fresh
 }
 
 #[cfg(test)]
@@ -56,11 +106,11 @@ mod tests {
 
     #[test]
     fn part1() {
-        assert_eq!(solve1(INPUT), 13);
+        assert_eq!(solve1(INPUT), 3);
     }
 
     #[test]
     fn part2() {
-        assert_eq!(solve2(INPUT), 43);
+        assert_eq!(solve2(INPUT), 14);
     }
 }
